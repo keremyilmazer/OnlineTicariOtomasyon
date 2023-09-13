d7c835f0a2b8f437052d115471f14c437cf793df
Kerem <keremyilmazerrr@hotmail.com>
2023-09-13 17:24:21 +0300

    Ürün Index sayfası

---
+++ MvcOnlineTicariOtomasyon/Controllers/UrunController.cs
@@ -0,0 +1,20 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var urunler=c.Uruns.ToList();
+            return View(urunler);
+        }
+    }
+}
