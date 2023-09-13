b97ecb877ccc15bbcb109df1944a25ecb833ce69
Kerem <keremyilmazerrr@hotmail.com>
2023-09-13 17:06:45 +0300

    Kategori güncelleme sayfası

--- MvcOnlineTicariOtomasyon/Controllers/KategoriController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/KategoriController.cs
@@ -10,11 +10,47 @@ namespace MvcOnlineTicariOtomasyon.Controllers
     public class KategoriController : Controller
     {
         // GET: Kategori
-        Context c = new Context(); 
+        Context c = new Context();
         public ActionResult Index()
         {
-            var degerler=c.Kategoris.ToList();
+            var degerler = c.Kategoris.ToList();
             return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult KategoriEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult KategoriEkle(Kategori k)
+        {
+            c.Kategoris.Add(k);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriSil(int id)
+        {
+            var ktg = c.Kategoris.Find(id);
+            c.Kategoris.Remove(ktg);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriGetir(int id)
+        {
+            var kategori = c.Kategoris.Find(id);
+            return View("KategoriGetir", kategori);
+        }
+
+        public ActionResult KategoriGuncelle(Kategori k)
+        {
+            var ktgr = c.Kategoris.Find(k.KategoriID);
+            ktgr.KategoriAd = k.KategoriAd;
+            c.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
