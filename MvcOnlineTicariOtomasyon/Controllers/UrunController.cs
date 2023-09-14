6ad53b67ed51a264ff8b46aae2ab833dc6795f3c
Kerem <keremyilmazerrr@hotmail.com>
2023-09-14 18:21:11 +0300

    Yeni ürün ekleme dropdownlistfor ile

--- MvcOnlineTicariOtomasyon/Controllers/UrunController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/UrunController.cs
@@ -20,6 +20,13 @@ namespace MvcOnlineTicariOtomasyon.Controllers
         [HttpGet]
         public ActionResult YeniUrun()
         {
+            List<SelectListItem> deger1 = (from x in c.Kategoris.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.KategoriAd,
+                                               Value = x.KategoriID.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
             return View();
         }
 
