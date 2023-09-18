5cb90b404f27a14416627d702728097d9af61a1e
Kerem <keremyilmazerrr@hotmail.com>
2023-09-18 15:54:08 +0300

    Personel Index sayfası

---
+++ MvcOnlineTicariOtomasyon/Controllers/PersonelController.cs
@@ -0,0 +1,20 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class PersonelController : Controller
+    {
+        // GET: Personel
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Personels.ToList();
+            return View(degerler);
+        }
+    }
+}
