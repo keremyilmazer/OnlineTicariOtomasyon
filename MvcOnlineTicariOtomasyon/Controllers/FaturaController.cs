de864b0bce0325a3298cf5ea84ffe35f5a7891a5
Kerem <keremyilmazerrr@hotmail.com>
2023-09-19 21:19:52 +0300

    Yeni fatura girişi

--- MvcOnlineTicariOtomasyon/Controllers/FaturaController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/FaturaController.cs
@@ -13,8 +13,20 @@ namespace MvcOnlineTicariOtomasyon.Controllers
         Context c = new Context();
         public ActionResult Index()
         {
-            var liste =c.Faturalars.ToList();
+            var liste = c.Faturalars.ToList();
             return View(liste);
+        }
+        [HttpGet]
+        public ActionResult FaturaEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult FaturaEkle(Faturalar f)
+        {
+            c.Faturalars.Add(f);
+            c.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
