e4647803913b06061565eade5de76870960595a3
Kerem <keremyilmazerrr@hotmail.com>
2023-09-20 16:24:45 +0300

    Linq sorgularla istatistik çekme 4

--- MvcOnlineTicariOtomasyon/Controllers/İstatistikController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/İstatistikController.cs
@@ -35,6 +35,16 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             ViewBag.d10 = deger10;
             var deger11 = c.Uruns.Count(x => x.UrunAd == "Laptop").ToString();
             ViewBag.d11 = deger11;
+
+            var deger14 = c.SatisHarekets.Sum(x => x.ToplamTutar).ToString();
+            ViewBag.d14 = deger14;
+
+            DateTime bugun = DateTime.Today;
+            var deger15 = c.SatisHarekets.Count(x => x.Tarih == bugun).ToString();
+            ViewBag.d15 = deger15;
+
+            var deger16 = c.SatisHarekets.Where(x => x.Tarih == bugun).Sum(y => y.ToplamTutar).ToString();
+            ViewBag.d16 = deger16;
             return View();
         }
     }
