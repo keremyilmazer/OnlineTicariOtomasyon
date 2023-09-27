d46797f0cfd36b038024c169cf81af2e915bc081
Kerem <keremyilmazerrr@hotmail.com>
2023-09-27 16:56:53 +0300

    Bir viewda birden fazla tablo listeleme

--- MvcOnlineTicariOtomasyon/Controllers/UrunDetayController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/UrunDetayController.cs
@@ -13,8 +13,11 @@ namespace MvcOnlineTicariOtomasyon.Controllers
         Context c = new Context();
         public ActionResult Index()
         {
-            var degerler = c.Uruns.Where(x => x.Urunid == 1).ToList();
-            return View(degerler);
+            Class1 cs = new Class1();
+            cs.Deger1 = c.Uruns.Where(x => x.Urunid == 1).ToList();
+            cs.Deger2 = c.Detays.Where(y => y.DetayID == 1).ToList();
+            //var degerler = c.Uruns.Where(x => x.Urunid == 1).ToList();
+            return View(cs);
         }
     }
 }
