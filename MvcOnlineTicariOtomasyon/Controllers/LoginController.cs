39241d868faea2c289534d3a1befe2d14e46d828
Kerem <keremyilmazerrr@hotmail.com>
2023-10-16 17:35:42 +0300

    Yeni Cari Kaydı

--- MvcOnlineTicariOtomasyon/Controllers/LoginController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,12 +10,22 @@ namespace MvcOnlineTicariOtomasyon.Controllers
     public class LoginController : Controller
     {
         // GET: Login
+        Context c = new Context();
         public ActionResult Index()
         {
             return View();
         }
+
+        [HttpGet]
         public PartialViewResult Partial1()
         {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult Partial1(Cariler p)
+        {
+            c.Carilers.Add(p);
+            c.SaveChanges();
             return PartialView();
         }
     }
