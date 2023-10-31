8eb24d083d617939a206241df277ec162dce7641
Kerem <keremyilmazerrr@hotmail.com>
2023-10-31 17:56:08 +0300

    Controller Tarafında Grafik Oluşturma

--- MvcOnlineTicariOtomasyon/Controllers/GrafikController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/GrafikController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace MvcOnlineTicariOtomasyon.Controllers
@@ -12,6 +13,12 @@ namespace MvcOnlineTicariOtomasyon.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult Index2()
+        {
+            var grafikciz = new Chart(600, 600);
+            grafikciz.AddTitle("Kategori - Ürün Stok Sayıları").AddLegend("Stok").AddSeries("Değerler", xValue: new[] { "Mobilya", "Ofis Eşyaları", "Bilgisayar" }, yValues: new[] { 85, 66, 98 }).Write();
+            return File(grafikciz.ToWebImage().GetBytes(), "image/jpeg");
         }
     }
 }
